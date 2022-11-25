30d3e130a2d9f558956a5a011630700131990990
Guilherme Scheffer <guilherme-scheffer@hotmail.com>
2022-11-25 17:50:57 -0300

    changes in design

--- src/componentes/Demigods/index.js
+++ src/componentes/Demigods/index.js
@@ -7,10 +7,12 @@ const Demigod = ({name, age, mythology}) => {
 
                 <h4>{age}</h4>
                 <h5>{name}</h5>
-
             </div>
-            <div className="mythology">   
-                
+            <div className="demigod-image">
+                <img src="/imgs/poseidon.png" alt="imagem poseidon"></img>   
+            </div>
+            <div className="about-card">
+                <p>{name}, filho do Deus "DEUS" tem poderes incriveis</p>
             </div>
         </div>
     )
